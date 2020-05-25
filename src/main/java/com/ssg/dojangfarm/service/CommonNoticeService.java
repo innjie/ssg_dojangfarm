80117b38f06776f4ca888124fb4233c5f87eb4ce
innjie <innjie@naver.com>
2020-05-25 21:30:04 +0900

    commonNotice Controller by innjie

--- src/main/java/com/ssg/dojangfarm/service/CommonNoticeService.java
+++ src/main/java/com/ssg/dojangfarm/service/CommonNoticeService.java
@@ -12,4 +12,5 @@ public interface CommonNoticeService {
 	public CommonNotice viewCommonNotice(CommonNotice cn);
 	public List<CommonNotice> getAllNoticeList();
 	public List<CommonNotice> getCNoticeListByUserNo(int userNo);
+	public CommonNotice getCN(String userId);
 }
